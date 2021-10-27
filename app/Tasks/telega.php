5b4ad80f43cac173204072dda420e5bcc26e9354
Evgeniy Petrikey <evgeniy.petrikey@appflame.com>
2021-10-27 17:37:09 +0300

    config d-c

--- app/Tasks/telega.php
+++ app/Tasks/telega.php
@@ -1,6 +1,7 @@
 <?php
 $interval = 180;
 $lastSendTime = time();
+$first = true;
 
 while (true) {
 
@@ -26,6 +27,7 @@ while (true) {
         $lastSendTime <= $lastMessage['message']['date'] &&
         $lastMessage['message']['date'] >= (time() - 1800)
     ) {
+        $first = true;
         echo " SKIP \n";
         continue;
     }
@@ -35,9 +37,23 @@ while (true) {
         620708083
     ];
 
+    $types = [11, 12, 13, 14, 15, 16, 18];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://rzhunemogu.ru/RandJSON.aspx?CType=" . $types[array_rand($types)]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    $output = iconv('cp1251', 'utf-8', $output);
+    $output = str_replace('{"content":', '', $output);
+    $message = str_replace('"}', '', $output);
+
+
+
     foreach ($chanenalIds as $id) {
         $url = "https://api.telegram.org/bot2050411951:AAFFpB6mbWYM-vc1At7x_cTjFbI23QJYuxk/sendMessage?chat_id=" . $id;
-        $url = $url . "&text=ДАЙ ВОДЫ СЫНУ (Если дала то ответь 'OK' или 'ДАЛА' или 'НЕ ЗАЕБУЙ')";
+        $url = $url . "&text=ДАЙ ВОДЫ СЫНУ и немного улыбнись)\n ----> " .$message . " \n (Если дала то ответь 'OK' или 'ДАЛА' или 'НЕ ЗАЕБУЙ')";
         $ch = curl_init();
         $optArray = [
             CURLOPT_URL => $url,
@@ -47,6 +63,7 @@ while (true) {
         curl_exec($ch);
         curl_close($ch);
 
+        $first = false;
     }
     $lastSendTime = time();
     echo " SEND \n";
